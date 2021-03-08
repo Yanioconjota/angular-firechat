0a0501e6b5cf93ca1cb6e07a3dcb252be1b8b875
Janio Isacura <janioisacura@gmail.com>
2021-03-08 17:57:19 -0300

    Instalación y configuración firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDxanPGaOPH4VSFNy2d-MFykXFius03Pjg',
+    authDomain: 'firechat-d484b.firebaseapp.com',
+    databaseURL: 'https://firechat-d484b-default-rtdb.firebaseio.com/',
+    projectId: 'firechat-d484b',
+    storageBucket: 'firechat-d484b.appspot.com',
+    messagingSenderId: '187751567226',
+    appId: '1:187751567226:web:1c4457daaabffd75f210c3',
+    measurementId: 'G-TM1J1Y0KH0'
+  }
 };
 
 /*
