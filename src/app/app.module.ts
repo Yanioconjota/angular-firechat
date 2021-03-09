cb1cd10dbeade22a2d828166e2ca43cb881f2092
Janio Isacura <janioisacura@gmail.com>
2021-03-09 19:51:39 -0300

    Se creó servicio de mostrar y crear mensajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -8,6 +10,7 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ChatService } from './providers/chat.service';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,15 @@ import { ChatComponent } from './components/chat/chat.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from 'src/app/providers/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ChatComponent implements OnInit {
+  mensaje: string = "";
 
-  constructor() { }
+  constructor(public _cs: ChatService ) {
+    this._cs.cargarMensajes()
+            .subscribe();
+  }
 
   ngOnInit(): void {
+  }
+
+  enviar_mensaje(mensaje: string) {
+    console.log(this.mensaje);
+    if (this.mensaje.length === 0) {
+      return;
+    }
+
+    this._cs.agregarMensaje(this.mensaje)
+            .then(() => {
+              console.log('Mensaje guardado');
+              this.mensaje = '';
+            })
+            .catch((err) => console.log('Error al enviar mensaje', err));
   }
 
 }
